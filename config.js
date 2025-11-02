2d0222ec7fca38850b207d18ea75e7f18ba2a65c
Senpaixdz <deadlordeditz12@gmail.com>
2025-11-02 13:43:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["440498587357413388", ""], 
   mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/luxuryz",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
